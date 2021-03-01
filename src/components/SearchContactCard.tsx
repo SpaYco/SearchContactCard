9b19e0e6c752dd7adc5419a4b5bb2fbfe6b96642
SpaYco <zizou.aziz101@gmail.com>
2021-03-01 20:21:02 +0100

    Fix Error

--- src/components/SearchContactCard.tsx
+++ src/components/SearchContactCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Grid, createMuiTheme, makeStyles } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
